20168ec3c660b4543841b9ca3b0533cb5c294426
Jared6060 <51765927+jared6060@users.noreply.github.com>
2026-03-25 00:10:48 -0400

    Update to 1.20.1
    Updated to Minecraft Forge version 47. Removed unnecessary mod constructor. @Override -> @Overwrite in the mixin (oops.) Refactored compute function for readability.

--- src/main/java/io/github/jared6060/steepmaterialconditionfix/mixin/MixinSteepMaterialCondition.java
+++ src/main/java/io/github/jared6060/steepmaterialconditionfix/mixin/MixinSteepMaterialCondition.java
@@ -1,6 +1,7 @@
 package io.github.jared6060.steepmaterialconditionfix.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.Heightmap;
@@ -12,28 +13,29 @@ public class MixinSteepMaterialCondition extends SurfaceRules.LazyXZCondition {
         super(p_189594_);
     }
 
-    @Override
+    @Overwrite
     protected boolean compute() {
-        int i = this.context.blockX & 15;
-        int j = this.context.blockZ & 15;
-        int k = Math.max(j - 1, 0);
-        int l = Math.min(j + 1, 15);
-        ChunkAccess chunkaccess = this.context.chunk;
-        int i1 = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, i, k);
-        int j1 = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, i, l);
+        ChunkAccess chunkAccess = this.context.chunk;
 
-        // The fix detects positive and negative slopes rather than just positive slopes
-        // along the x-axis.
-        if (Math.abs(i1 - j1) >= 4)
+        int targetX = this.context.blockX & 15;
+        int targetZ = this.context.blockZ & 15;
+
+        int targetSouthZ = Math.max(targetZ - 1, 0);
+        int targetNorthZ = Math.min(targetZ + 1, 15);
+
+        int targetSouthY = chunkAccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, targetX, targetSouthZ);
+        int targetNorthY = chunkAccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, targetX, targetNorthZ);
+
+        if (Math.abs(targetSouthY - targetNorthY) >= 4) {
             return true;
+        }
 
-        int k1 = Math.max(i - 1, 0);
-        int l1 = Math.min(i + 1, 15);
-        int i2 = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, k1, j);
-        int j2 = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, l1, j);
+        int targetWestX = Math.max(targetX - 1, 0);
+        int targetEastX = Math.min(targetX + 1, 15);
 
-        // The fix detects positive and negative slopes rather than just positive slopes
-        // along the z-axis.
-        return Math.abs(i2 - j2) >= 4;
+        int targetWestY = chunkAccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, targetWestX, targetZ);
+        int targetEastY = chunkAccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, targetEastX, targetZ);
+
+        return Math.abs(targetWestY - targetEastY) >= 4;
     }
 }
