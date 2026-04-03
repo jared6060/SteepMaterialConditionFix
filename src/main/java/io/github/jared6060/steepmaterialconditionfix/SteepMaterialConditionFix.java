4d0dbdd9d6fd450d092647ca1d76b95a4e89a393
Jared6060 <51765927+jared6060@users.noreply.github.com>
2026-04-03 00:05:00 -0400

    Update to 1.21.1 NeoForge
    Rebuilt project from NeoForge's example mod

--- src/main/java/io/github/jared6060/steepmaterialconditionfix/SteepMaterialConditionFix.java
+++ src/main/java/io/github/jared6060/steepmaterialconditionfix/SteepMaterialConditionFix.java
@@ -1,13 +1,8 @@
 package io.github.jared6060.steepmaterialconditionfix;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
 @Mod(SteepMaterialConditionFix.MODID)
 public class SteepMaterialConditionFix {
     public static final String MODID = "steepmaterialconditionfix";
-
-    public SteepMaterialConditionFix() {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
 }
